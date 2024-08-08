4eebfc3d31f3677bb76f0f10d0224de56f4931e8
vishnu <vishnumeloth@gmail.com>
2024-08-08 19:51:12 +0530

    final commit before review

--- lib/features/inventory/UI/widgets/custom_grid.dart
+++ lib/features/inventory/UI/widgets/custom_grid.dart
@@ -3,6 +3,7 @@ import 'package:adast_seller/%20themes/themes.dart';
 import 'package:adast_seller/methods/common_methods.dart';
 import 'package:adast_seller/models/cloth_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -15,6 +16,7 @@ class CustomGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width=kIsWeb?MediaQuery.of(context).size.width * 0.163:MediaQuery.of(context).size.width * 0.463;
     return Padding(
       padding: const EdgeInsets.only(top: 1, left: 1),
       child: Container(
@@ -28,7 +30,7 @@ class CustomGrid extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             SizedBox(
-              width: MediaQuery.of(context).size.width * 0.463,
+              width: width,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(5),
                 child: CachedNetworkImage(
@@ -39,14 +41,14 @@ class CustomGrid extends StatelessWidget {
                     highlightColor: Colors.grey[100]!,
                     child: Container(
                       color: Colors.grey[300],
-                      height: MediaQuery.of(context).size.width * 0.463*1.5,
+                      height: width*1.5,
                     ),
                   ),
                   errorWidget: (context, url, error) => const Icon(Icons.error),
                 ),
               ),
             ),SizedBox(
-              width: MediaQuery.of(context).size.width * 0.463,
+              width: width,
               child: Text(
                 overflow: TextOverflow.ellipsis,
                 clothModel.name,
